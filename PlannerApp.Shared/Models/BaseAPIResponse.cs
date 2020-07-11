e9f57501da7e1df56b8a1b611600938be7ff72dd
bartvanhoey <bartvanhoey@hotmail.com>
2020-07-11 18:20:59 +0200

    end-25

--- PlannerApp.Shared/Models/BaseAPIResponse.cs
+++ PlannerApp.Shared/Models/BaseAPIResponse.cs
@@ -2,7 +2,7 @@ namespace PlannerApp.Shared.Models
 {
   public abstract class BaseAPIResponse
   {
-    string Message { get; set; }
-    bool IsSuccess { get; set; }
+    public string Message { get; set; }
+    public bool IsSuccess { get; set; }
   }
 }
